25eef71731170ebf14c964d2fa5e021961b99d83
Ammar Rahic <ammarrahic@live.com>
2019-06-22 23:07:38 +0200

    Add clear button

--- src/App.js
+++ src/App.js
@@ -35,12 +35,18 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  clearUsers = () => this.setState({ users: [], loading: false });
+
   render() {
     return (
       <div>
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -7,7 +7,9 @@ class Search extends Component {
   };
 
   static propTypes = {
-    searchUsers: PropTypes.func.isRequired
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   };
 
   onSubmit = e => {
@@ -19,6 +21,8 @@ class Search extends Component {
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    const { showClear, clearUsers } = this.props;
+
     return (
       <div>
         <form onSubmit={this.onSubmit} className='form'>
@@ -32,9 +36,14 @@ class Search extends Component {
           <input
             className='btn btn-dark btn-block'
             type='submit'
-            value='search'
+            value='Search'
           />
         </form>
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
